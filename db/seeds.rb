743c80ef651bd49078fe2c7517471bfb5619f6cb
vagrant <vagrant@rails-dev-box>
2014-10-05 23:06:12 +0000

    Seeded users, posts and comments.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
 require 'faker'
  
+
+# Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
  # Create Posts
  50.times do
    Post.create!(
+     user:   users.sample,
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
@@ -12,11 +26,18 @@ require 'faker'
  # Create Comments
  100.times do
    Comment.create!(
+     # user: users.sample,
      post: posts.sample,
      body: Faker::Lorem.paragraph
    )
  end
- 
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+
+ User.first.update_attributes!(
+  email: 'rachelb@gmail.com',
+  password: 'thisisatest',
+)
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
