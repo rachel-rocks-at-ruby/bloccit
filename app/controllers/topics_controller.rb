8caa3ceafcb1ead2e224999ba071450f41eeca02
vagrant <vagrant@rails-dev-box>
2014-10-26 23:10:06 +0000

    Created public profiles

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -21,7 +21,7 @@ class TopicsController < ApplicationController
   end
  
    def create
-     @topic = Topic.new(params.require(topic_params))
+     @topic = Topic.new(topic_params)
      authorize @topic
      if @topic.save
        redirect_to @topic, notice: "Topic was saved successfully."
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
-   before_action :authenticate_user!
+   before_action :authenticate_user!, except: [:show]
  
+   def show
+     @user = User.find(params[:id])
+     @posts = @user.posts.visible_to(current_user)
+     @comments = @user.comments
+   end
+
    def update
      if current_user.update_attributes(user_params)
        flash[:notice] = "User information updated"
@@ -10,7 +16,7 @@ class UsersController < ApplicationController
        redirect_to edit_user_registration_path
      end
    end
- 
+
    private
  
    def user_params
