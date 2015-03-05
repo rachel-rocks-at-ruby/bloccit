998488532b871ff190684c3c09a9bbcc360f25b7
vagrant <rachelb@gmail.com>
2015-03-05 01:46:16 +0000

    Added action_mailer configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'rachel-bloccit.heroku.com' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
 end
